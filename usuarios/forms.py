3372a0c8eb474ea63e6b3d4d4798d19a79b4b5c6
Swagets <alejandroproao97@gmail.com>
2026-07-11 22:12:18 -0500

    Cifrado de contrasenia

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -10,5 +10,26 @@ class RegistroUsuarioForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'username', 'email', 'password', 'rol']
         widgets = {
             'rol': forms.Select(attrs={'class': 'form-control'}),
-            # Agrega más clases de Bootstrap a los otros campos aquí
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
         }
+
+    # 1. Validar que las contraseñas coincidan
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Las contraseñas no coinciden.")
+        return cleaned_data
+
+    # 2. Guardar de forma segura
+    def save(self, commit=True):
+        usuario = super().save(commit=False)
+        usuario.set_password(self.cleaned_data['password'])
+        if commit:
+            usuario.save()
+        return usuario
